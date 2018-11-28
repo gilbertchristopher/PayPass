9a7918e5d819e346835992b09a523aea3a768ade
anthonyant97 <anthonyant97@gmail.com>
2018-11-28 20:15:08 +0700

    change String to string

--- src/data/buyer.interface.ts
+++ src/data/buyer.interface.ts
@@ -1,13 +1,13 @@
 import { User } from './user.interface';
 
 export interface Buyer{
-    id: String;
-    first_name: String;
-    last_name: String;
+    id: string;
+    first_name: string;
+    last_name: string;
     dateOfBirth: Date;
-    address: String;
-    buyerCity: String;
-    buyerRegency: String;
-    buyerPhone: String;
+    address: string;
+    buyerCity: string;
+    buyerRegency: string;
+    buyerPhone: string;
     user: User;
 }
--- src/data/user.interface.ts
+++ src/data/user.interface.ts
@@ -1,6 +1,6 @@
 export interface User{
-    id: String;
-    email: String;
-    password: String;
-    role: String;
+    id: string;
+    email: string;
+    password: string;
+    role: string;
 }
