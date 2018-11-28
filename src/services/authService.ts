33b61a694ed7bbe943bcc6e41c7721d282c591c6
anthonyant97 <anthonyant97@gmail.com>
2018-11-28 17:27:29 +0700

    test db signup

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,8 +1,14 @@
 import firebase from "firebase";
+import { User } from "../data/user.interface";
 
 export class AuthService {
-    signup(email: string, password: string) {
-        return firebase.auth().createUserWithEmailAndPassword(email, password);
+    signup(email: string, password: string, user: User) {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((userData) => {
+                console.log("user ", userData);
+            }).catch((error) => {
+                console.log("error ", error);
+            });
     }
 
     signin(email: string, password: string) {
