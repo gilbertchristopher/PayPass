873c39761c7df60e883680c232f32c4b7eab1f78
anthonyant97 <anthonyant97@gmail.com>
2018-11-28 20:16:19 +0700

    try signup to firebase

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,11 +1,24 @@
 import firebase from "firebase";
 import { User } from "../data/user.interface";
+import { Buyer } from "../data/buyer.interface";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+@Injectable()
 export class AuthService {
-    signup(email: string, password: string, user: User) {
-        firebase.auth().createUserWithEmailAndPassword(email, password)
+    constructor(private http: HttpClient){
+
+    }
+
+    signup(email: string, password: string, user: User, buyer: Buyer) {
+        return firebase.auth().createUserWithEmailAndPassword(email, password)
             .then((userData) => {
-                console.log("user ", userData);
+                console.log("user before ", user);
+                user.id = userData.user.uid;
+                user.role = "Buyer";
+                console.log("user after ", user);
+                // buyer.user = user;
+                this.storeUser(user);
             }).catch((error) => {
                 console.log("error ", error);
             });
@@ -21,5 +34,11 @@ export class AuthService {
 
     getActiveUser() {
         return firebase.auth().currentUser;
+    }
+    
+    storeUser(user: User) {  
+        console.log("store ", user.id);
+        return this.http.put("https://paypass-id.firebaseio.com/" + user.id + "/userInfo", user);
+        // return this.http.put("https://paypass-id.firebaseio.com/" + user.id + "/buyerInfo", buyer);
     }
 }
