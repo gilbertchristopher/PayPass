3638fdb41b6f9b34e106449b5fff6af3d1880483
anthonyant97 <anthonyant97@gmail.com>
2018-11-28 20:15:53 +0700

    add httpclientmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import { AuthService } from '../services/authService';
 import { ProductService } from '../services/productService';
 import { StoreService } from '../services/storeService';
 import { Loc } from '../services/location';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -48,6 +49,7 @@ import { Loc } from '../services/location';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     // IonicModule.forRoot(MyApp)  default
     IonicModule.forRoot(MyApp, {
       scrollPadding: false,
