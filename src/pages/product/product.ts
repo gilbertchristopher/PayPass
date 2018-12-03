085d178b46e82c478f82d1edef0e47f9cf21bee5
ariefwdyto <arief.widyanto21@gmail.com>
2018-12-03 13:25:39 +0700

    UserService di product siap pake

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AddProductPage } from '../add-product/add-product';
 import { EditProductPage } from '../edit-product/edit-product';
+import { UserService } from '../../services/buyerService';
 
 
 @IonicPage()
@@ -11,8 +12,11 @@ import { EditProductPage } from '../edit-product/edit-product';
 })
 export class ProductPage {
   isSearchbarOpened = false;
+  sellerData: any;
+  
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private buyerService:UserService) {
+    this.sellerData = this.buyerService.getUserData();
   }
 
   ionViewDidLoad() {
