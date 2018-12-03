e887b36aa0e76320b9d5664bbb1e31fc43e4e597
Gilbert Christopher <gilbert.christopher@student.umn.ac.id>
2018-12-03 15:34:50 +0700

    Products

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AddProductPage } from '../add-product/add-product';
 import { EditProductPage } from '../edit-product/edit-product';
 import { UserService } from '../../services/buyerService';
+import { ProductStore } from '../../data/productstore.interface';
 
 
 @IonicPage()
@@ -12,11 +13,26 @@ import { UserService } from '../../services/buyerService';
 })
 export class ProductPage {
   isSearchbarOpened = false;
-  sellerData: any;
-  
+  sellerData : any;
+  productData: ProductStore;
+  productsData: ProductStore[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private buyerService:UserService) {
     this.sellerData = this.buyerService.getUserData();
+    console.log(this.sellerData);
+    console.log(this.sellerData.products);
+    for(let product of this.sellerData.products){
+      this.productData.price = product.price;
+      this.productData.qty = product.qty;
+      this.productData.product.productName = product.product.name;
+      this.productData.product.productDesc = product.product.desc;
+      this.productData.product.id = product.key;
+      this.productsData.push(this.productData)
+    }
+    for(let product of this.productsData){
+      console.log(product)
+    }
+    // console.log(this.sellerData.products[]);
   }
 
   ionViewDidLoad() {
