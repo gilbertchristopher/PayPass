8330742e554620970ce895233173d492fc2bf496
anthonyant97 <anthonyant97@gmail.com>
2018-12-04 15:51:49 +0700

    add method store productstore

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -2,12 +2,15 @@ import { Product } from '../data/product.interface';
 import { AuthService } from './authService';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import firebase from 'firebase';
+import { ProductStore } from '../data/productstore.interface';
+import { LoadingController } from 'ionic-angular';
 
 @Injectable()
 export class ProductService {
     private productData: Product[] = [];
 
-    constructor(private http: HttpClient, private authSvc: AuthService) {
+    constructor(private http: HttpClient, private authSvc: AuthService, private loadingCtrl: LoadingController) {
     }
 
     addNewStore(product: Product){
@@ -20,6 +23,24 @@ export class ProductService {
 
     getAllproduct(){
         return this.productData;
+    }
+
+    addProductToStore(product: ProductStore, id: string) {
+        let uid = this.authSvc.getActiveUser().uid;
+        let loading = this.loadingCtrl.create({
+            spinner: 'circles',
+            content: 'Loading, Please Wait...',
+            dismissOnPageChange: true,
+        });
+      
+        loading.present();
+        const productRef: firebase.database.Reference = firebase.database().ref('user/' + uid + '/products/' + id);
+
+        productRef.update(product).then(res => {
+            loading.dismiss();
+        }).catch(err => {
+            loading.dismiss();
+        })
     }
 
     // productList(token: string) {
