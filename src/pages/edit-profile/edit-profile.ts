5915b6394d31eb6b30341ec311b00176bca8d52e
anthonyant97 <anthonyant97@gmail.com>
2018-12-09 17:05:14 +0700

    can update photo profile

--- src/pages/edit-profile/edit-profile.ts
+++ src/pages/edit-profile/edit-profile.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { Component, OnInit, Input } from '@angular/core';
+import { IonicPage, NavController, NavParams, ModalController, ActionSheetController, LoadingController } from 'ionic-angular';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { User } from 'firebase';
 import { AuthService } from '../../services/authService';
 import { UserService } from '../../services/buyerService';
 import { ChooseLocationPage } from '../choose-location/choose-location';
 import { Loc } from '../../services/location';
-
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -23,15 +23,22 @@ export class EditProfilePage implements OnInit {
   role: string;
   lat = -6.178306;
   lng = 106.631889;
-  
+  base64Url: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private authService: AuthService,  private userService: UserService, private modalCtrl: ModalController) {
+  @Input('useURI') useURI: boolean = true;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private authService: AuthService,  private userService: UserService, 
+          private modalCtrl: ModalController, private camera: Camera, private actionSheetCtrl: ActionSheetController, private loadingCtrl: LoadingController) {
     this.buyerData = this.userService.getUserData();
     this.role = this.buyerData.role;
     this.marker = new Loc();
     this.lat = this.buyerData.lat;
     this.lng = this.buyerData.lng;
     this.marker.setLocation(this.lat, this.lng);
+  }
+
+  ionViewWillEnter() {
+    // this.base64Url = this.buyerData.profile;
   }
 
   ngOnInit(){
@@ -78,5 +85,84 @@ export class EditProfilePage implements OnInit {
     this.navCtrl.pop();
   }
 
+  editProfile() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Choose image',
+      buttons: [
+        {
+          text: 'Take a Photo',
+          handler: () => {
+            this.getPicture(1);
+          }
+        },
+        {
+          text: 'Select From Gallery',
+          handler: () => {
+            this.getPicture(0);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ],
+      enableBackdropDismiss: false
+    });
+ 
+    actionSheet.present();
+  }
 
+  getPicture(sourceType){
+    const cameraOptions: CameraOptions = {
+      quality: 50, 
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: sourceType
+    };
+
+    this.camera.getPicture(cameraOptions)
+     .then((base64Url) => {
+      let loader = this.loadingCtrl.create({
+        spinner: 'circles',
+        content: 'Updating your profile'
+      });
+      loader.present();
+      //  this.base64Url = 'data:image/jpeg;base64,' + base64Url;
+       this.userService.uploadPhotoUser(base64Url, this.buyerData.role.toLowerCase()).then(() => {
+        this.buyerData = this.userService.getUserData();
+        loader.dismiss();
+       });
+    }, (err) => {
+        console.log(err);
+    });
+  }  
+  // uploadPhoto() {
+  //   let storageRef = firebase.storage().ref();
+  //   // Create a timestamp as filename
+  //   const filename = Math.floor(Date.now() / 1000);
+
+  //   // Create a reference to 'images/todays-date.jpg'
+  //   const imageRef = storageRef.child(`images/${filename}.jpg`);
+
+  //   imageRef.putString(this.base64Url, firebase.storage.StringFormat.DATA_URL)
+  //     .then((snapshot)=> {
+  //       // Do something here when the data is succesfully uploaded!
+  //       this.showSuccesfulUploadAlert();
+  //   });
+  // }
+
+  // showSuccesfulUploadAlert() {
+  //   let alert = this.alertCtrl.create({
+  //     title: 'Uploaded!',
+  //     subTitle: 'Picture is uploaded to Firebase',
+  //     buttons: ['OK']
+  //   });
+  //   alert.present();
+  //   // clear the previous photo data in the variable
+  //   this.base64Url = "";
+  // }
 }
