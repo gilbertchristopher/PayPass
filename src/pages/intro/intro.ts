7278a17ccacb238315ea04773188fc5950f47fdd
Gilbert Christopher <gilbert.christopher@student.umn.ac.id>
2018-12-12 17:12:28 +0700

    WIP : Slider page 2

--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import { LoginPage } from '../login/login';
 import * as $ from 'jquery';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
 
 /**
  * Generated class for the IntroPage page.
@@ -17,17 +19,30 @@ import * as $ from 'jquery';
   templateUrl: 'intro.html',
 })
 export class IntroPage {
+  @ViewChild(Slides) slides: Slides;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    $(function(){
-      setTimeout(function(){
+    $(function () {
+      setTimeout(function () {
         $(".fly-in-item-form").removeClass('hidden')
       }, 500);
-    })
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad IntroPage');
+  }
+
+  slideChanged(){
+    let currentIndex = this.slides.getActiveIndex();
+    //console.log(currentIndex);
+    if(currentIndex == 1){
+      $(function () {
+        setTimeout(function () {
+          $(".fly-in").removeClass('hidden')
+        }, 500);
+      });
+    }
   }
 
   navHome() {
