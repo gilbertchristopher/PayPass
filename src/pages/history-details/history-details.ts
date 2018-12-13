3056e8e01a0ff6cf27c443c7cd167dc9604e7ca3
Gilbert Christopher <gilbert.christopher@student.umn.ac.id>
2018-12-13 07:32:53 +0700

    HistorySeller

--- src/pages/history-details/history-details.ts
+++ src/pages/history-details/history-details.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { UserService } from '../../services/buyerService';
 
 
 @IonicPage()
@@ -9,10 +10,52 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class HistoryDetailsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController) {
+  transactionData: any;
+  transactionId: string;
+  storeId: string;
+  productData: any[];
+  total: number;
+  totalPerProduct: number;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController, private userService: UserService) {
+    this.transactionId = this.navParams.get('transactionId');
+    this.storeId = this.navParams.get('storeId');
+    
+    this.total = 0;
+    this.totalPerProduct = 0;
+
+    if (this.transactionId != null && this.storeId != null) {
+      this.userService.getTransactionData({"transactionId": this.transactionId, "storeId":this.storeId}).then((data) => {
+        this.transactionData = data;
+        console.log(this.transactionData);
+        console.log(this.storeId);
+        this.productData = [];
+
+        for (let i in this.transactionData.products) {
+          this.totalPerProduct = (this.transactionData.products[i].qty * this.transactionData.products[i].price)
+          this.transactionData.products[i].totalPerProduct = this.totalPerProduct;
+          this.productData.push(this.transactionData.products[i]);
+          this.total = this.total + +this.totalPerProduct;
+        }
+        console.log(this.total)
+      })
+    }
+    else {
+      this.transactionData = this.navParams.data;
+      this.total = 0;
+      this.productData = [];
+      for (let i in this.transactionData.products) {
+        this.totalPerProduct = (this.transactionData.products[i].qty * this.transactionData.products[i].price)
+        this.transactionData.products[i].totalPerProduct = this.totalPerProduct;
+        this.productData.push(this.transactionData.products[i]);
+        this.total = this.total + +this.totalPerProduct;
+
+      }
+      console.log(this.total)
+    }
   }
 
-  dismiss(){
+  ondismiss(){
     this.viewCtrl.dismiss();
   }
 }
--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -17,23 +17,35 @@ export class HistoryPage {
   userData: any;
   productsData: any[];
   items: any[];
+  transactions: any[];
 
   constructor(public navCtrl: NavController, public authService: AuthService, public navParams: NavParams, private buyerService: UserService, private modalCtrl: ModalController) {
     this.userData = this.buyerService.getUserData();
-    this.items = [];
-    let foo = this.userData.transactions;
-    for(let i in foo){
-        foo[i].id = i;
-        this.items.push({"id": i, "firstname": foo[i].buyerInfo.firstname, "lastname": foo[i].buyerInfo.lastname, "date": foo[i].date, "time": foo[i].time, "status": foo[i].status});
-    }
+    //console.log(this.userData.role);
+    // this.items = [];
+    // let foo = this.userData.transactions;
+    // for(let i in foo){
+    //     foo[i].id = i;
+    //     this.items.push({"id": i, "firstname": foo[i].buyerInfo.firstname, "lastname": foo[i].buyerInfo.lastname, "date": foo[i].date, "time": foo[i].time, "status": foo[i].status});
+    // }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HistoryPage');
   }
 
-  historyDetails() {
-    let storeInformationModal = this.modalCtrl.create(HistoryDetailsPage, this.transactionData);
+  ionViewWillEnter() {
+    this.buyerService.getAllTransactionData().then((data) => {
+      this.transactions = [];
+      for(let i in data){
+        data[i].id = i;
+        this.transactions.push(data[i]);
+      }
+    })
+  }
+
+  historyDetails(transactions: any) {
+    let storeInformationModal = this.modalCtrl.create(HistoryDetailsPage, transactions);
     storeInformationModal.present();
   }
 
